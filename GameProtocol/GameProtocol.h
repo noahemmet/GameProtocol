feecd770cfd6cd5756c02e5582710453af521884
Noah Emmet <noah@noahemmet.com>
2015-08-09 17:15:46 -0700

    Initial Commit

---
+++ GameProtocol/GameProtocol.h
@@ -0,0 +1,19 @@
+//
+//  GameProtocol.h
+//  GameProtocol
+//
+//  Created by Noah Emmet on 8/9/15.
+//  Copyright © 2015 Sticks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GameProtocol.
+FOUNDATION_EXPORT double GameProtocolVersionNumber;
+
+//! Project version string for GameProtocol.
+FOUNDATION_EXPORT const unsigned char GameProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameProtocol/PublicHeader.h>
+
+
